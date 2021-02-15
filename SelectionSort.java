c591b105b0871cc2bd3e1591e99e9cbc30fd0910
Maffergu <60632469+Maffergu@users.noreply.github.com>
2021-02-14 19:48:47 -0600

    Add files via upload

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,26 +1,26 @@
-//Universidad del Valle de Guatemala.
-//Nombre: Fernanda Argueta y Pablo Gonzalez.
-//Hoja de trabajo 3
-//Clase para ordenar datos con la metodologia de Selectionsort
-//Referencia: Mishra, R. (2014, January 31). Selection Sort - GeeksforGeeks. Retrieved from GeeksforGeeks website: https://www.geeksforgeeks.org/selection-sort/
 class SelectionSort 
 { 
-    void selecsort(int arr[]) 
+    void sort(int arr[]) 
     { 
-        int n = arr.length; //Tamaño del array
+        int n = arr.length; 
+  
+        // One by one move boundary of unsorted subarray 
         for (int i = 0; i < n-1; i++) 
-        { // Encuentra el valor minimo del elemento en un array 
+        { 
+            // Encuentra el valor minimo del elemento en un array 
             int min_idx = i; 
             for (int j = i+1; j < n; j++) 
                 if (arr[j] < arr[min_idx]) 
                     min_idx = j; 
   
-            // Cambia el elemento menor con el primer elemento 
+            // Swap the found minimum element with the first 
+            // element 
             int temp = arr[min_idx]; 
             arr[min_idx] = arr[i]; 
             arr[i] = temp; 
         } 
-    }  
+    } 
+  
     // Imprime el array ingresada
     void printArray(int arr[]) 
     { 
--- main.java
+++ main.java
@@ -4,15 +4,24 @@ import java.io.FileReader;
 import java.io.FileWriter; // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
 import java.io.FileNotFoundException;
+import java.util.*;
 
 public class main {
   public static void main(String[] args) throws FileNotFoundException {
+    Scanner scan = new Scanner(System.in); //Variable asignada para el Scanner.
+    SelectionSort s=new SelectionSort();
+    GnomeSort g = new GnomeSort();
+    ArrayList<String> miarray = new ArrayList<String>();
+    ArrayList<Integer> array2 = new ArrayList<Integer>();
+    int[] saved  = new int[3000];
+    
     try {
       FileWriter myWriter = new FileWriter("filename.txt");
       for(int i=0;i<3000;i++){
         int valordado= (int) Math.floor(Math.random() * 3000+ 1);
         String a=String.valueOf(valordado);
         myWriter.write(a);
+        myWriter.write(" ");
       }
       myWriter.close();
       System.out.println("Successfully wrote to the file.");
@@ -21,8 +30,52 @@ public class main {
       e.printStackTrace();
     }
     archivos arch=new archivos();
-    String a=arch.leer("filename.txt");
-    System.out.println(a);
+    String ab=arch.leer("filename.txt");
+    String [] dividido=ab.split(" ");
+    for (String a : dividido)
+            miarray.add(a);
+    for(int i=0;i<3000;i++){
+        String a=miarray.get(i);
+        int numero=Integer.parseInt(a);
+        array2.add(numero);
+        int numero2=array2.get(i);
+        saved[i]=numero2;
+        
+        
+        
+    }
+    int op = 0;
+    while (op != 5){
+      System.out.println("Ingrese el número del tipo de sorting que desea utilizar");
+      System.out.println("1. Selection Sort ");
+      System.out.println("2. Gnome Sort ");
+      System.out.println("3. Merge Sort ");
+      System.out.println("4. Radix Sort ");
+
+      op = scan.nextInt();
+      if(op == 1){
+        s.sort(saved);
+        s.printArray(saved);
+      }else if(op == 2){
+        g.sort(saved);
+        g.print(saved);
+      }else if(op == 3){
+        //m.sort(saved);
+        //m.print(saved);
+      }else if(op == 4){
+        //r.sort(saved);
+        //r.print(saved);
+      }
+
+      
+    }
+    
+
+
+    
+
+
+    
     
   }
 }
